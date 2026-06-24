6b3934500b9a038994d3ba4a9048677c20382c7c
AvnishRao11 <avnishrao1122@gmail.com>
2026-06-24 18:22:08 +0530

    changed gemini model

--- Backend/src/services/ai.services.js
+++ Backend/src/services/ai.services.js
@@ -46,7 +46,7 @@ export async function generateInterviewReport({ resume, selfDescription, jobDesc
                     Job Description: ${jobDescription}`
 
     const response = await ai.models.generateContent({
-        model: "gemini-2.5-flash",
+        model: "gemini-3-flash-preview",
         contents: prompt,
         config: {
             responseMimeType: "application/json",
